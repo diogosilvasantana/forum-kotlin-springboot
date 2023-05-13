2b0b97c0b118d7968a3891b40c4c1f618397f997
Diogo Silva <diogosilvaoficial@gmail.com>
2023-05-13 00:44:52 -0300

    Post

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -7,59 +7,7 @@ import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
-class TopicoService(private var topicos: List<Topico>) {
-
-    init {
-        val topico1 = Topico(
-                id = 1,
-                titulo = "Dúvidas Kotlin 1",
-                mensagem = "Variáveis no Kotlin",
-                curso = Curso(
-                        id = 1,
-                        nome =  "Kotlin",
-                        categoria = "Programação"
-                ),
-                autor = Usuario(
-                        id = 1,
-                        nome = "Diogo Silva",
-                        email = "diogosilvaoficial@gmail.com"
-                )
-        )
-
-        val topico2 = Topico(
-                id = 2,
-                titulo = "Dúvidas Kotlin 2",
-                mensagem = "Variáveis no Kotlin",
-                curso = Curso(
-                        id = 1,
-                        nome =  "Kotlin",
-                        categoria = "Programação"
-                ),
-                autor = Usuario(
-                        id = 1,
-                        nome = "Diogo Silva",
-                        email = "diogosilvaoficial@gmail.com"
-                )
-        )
-
-        val topico3 = Topico(
-                id = 3,
-                titulo = "Dúvidas Kotlin 3",
-                mensagem = "Variáveis no Kotlin",
-                curso = Curso(
-                        id = 1,
-                        nome =  "Kotlin",
-                        categoria = "Programação"
-                ),
-                autor = Usuario(
-                        id = 1,
-                        nome = "Diogo Silva",
-                        email = "diogosilvaoficial@gmail.com"
-                )
-        )
-
-        topicos =  Arrays.asList(topico1, topico2, topico3)
-    }
+class TopicoService(private var topicos: List<Topico> = ArrayList()) {
 
     fun listar(): List<Topico> {
         return topicos
@@ -69,6 +17,10 @@ class TopicoService(private var topicos: List<Topico>) {
         return topicos.stream().filter({
             topico -> topico.id == id
         }).findFirst().get()
+    }
+
+    fun cadastrar(topico: Topico) {
+        topicos.plus(topico)
     }
 
 }
