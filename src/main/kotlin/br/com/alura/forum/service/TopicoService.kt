d7e48075ee27d4b5fdedd930166cf3d84f1066a8
Diogo Silva <diogosilvaoficial@gmail.com>
2023-05-14 00:05:39 -0300

    Mappings

--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -2,31 +2,22 @@ package br.com.alura.forum.service
 
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
+import br.com.alura.forum.mapper.TopicoFormMapper
+import br.com.alura.forum.mapper.TopicoViewMapper
 import br.com.alura.forum.model.Topico
 import org.springframework.stereotype.Service
 import java.util.*
 import java.util.stream.Collectors
 
 @Service
-class TopicoService(
-        private var topicos: List<Topico> = ArrayList(),
-        private val cursoService: CursoService,
-        private val usuarioService: UsuarioService
-) {
+class TopicoService(private var topicos: List<Topico> = ArrayList(), private val topicoViewMapper: TopicoViewMapper, private val topicoFormMapper: TopicoFormMapper) {
 
     fun listar(): List<TopicoView> {
-        return topicos.stream().map{topico -> TopicoView(
-                id = topico.id,
-                titulo = topico.titulo,
-                mensagem = topico.mensagem,
-                status = topico.status,
-                dataCriacao = topico.dataCriacao,
-
-        )}.collect(Collectors.toList())
+        return topicos.stream().map { topico -> topicoViewMapper.map(topico) }.collect(Collectors.toList())
     }
 
     fun buscarPorId(id: Long): TopicoView {
-        val topico =  topicos.stream().filter { topico ->
+        val topico = topicos.stream().filter { topico ->
             topico.id == id
         }.findFirst().get()
 
@@ -39,13 +30,9 @@ class TopicoService(
         )
     }
 
-    fun cadastrar(dto: NovoTopicoForm) {
-        topicos = topicos.plus(Topico(
-                id = topicos.size.toLong() + 1,
-                titulo = dto.titulo,
-                mensagem = dto.mensagem,
-                curso = cursoService.buscarPorId(dto.idCurso),
-                autor = usuarioService.buscarPorId(dto.idAutor)
-        ))
+    fun cadastrar(form: NovoTopicoForm) {
+        val topico = topicoFormMapper.map(form)
+        topico.id = topicos.size.toLong() + 1
+        topicos = topicos.plus(topico)
     }
 }
