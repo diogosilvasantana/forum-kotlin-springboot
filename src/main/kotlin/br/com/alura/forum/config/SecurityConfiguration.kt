5d83af30b0d7c7cc7e02acb8b54a37f237d9f972
Diogo Silva <diogosilvaoficial@gmail.com>
2023-05-27 00:09:11 -0300

    Add JWT

--- src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
@@ -1,28 +1,41 @@
 package br.com.alura.forum.config
 
+import br.com.alura.forum.security.JWTLoginFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration() {
+class SecurityConfiguration(
+        private val configuration: AuthenticationConfiguration,
+        private val jwtUtil: JWTUtil
+) {
     @Bean
-    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+    fun configure(http: HttpSecurity): SecurityFilterChain {
         http.
         authorizeHttpRequests().
-        anyRequest().
+        //requestMatchers("/topicos")?.hasAuthority("SOMENTE_LEITURA")?.
+        requestMatchers("/login")?.permitAll()?.
+        anyRequest()?.
         authenticated()
-                .and().
-                sessionManagement().
-                sessionCreationPolicy(SessionCreationPolicy.STATELESS).
-                and().
-                formLogin().disable().httpBasic()
+
+        http.addFilterBefore(JWTLoginFilter(authManager = configuration.authenticationManager, jwtUtil = jwtUtil), UsernamePasswordAuthenticationFilter().javaClass)
+
+        http.sessionManagement()?.
+        sessionCreationPolicy(SessionCreationPolicy.STATELESS)?.
+        and()?.
+        formLogin()?.disable()?.
+        httpBasic()
+
+        http.csrf().disable()
 
         return http.build()
     }
--- src/main/kotlin/br/com/alura/forum/service/UserDetail.kt
+++ src/main/kotlin/br/com/alura/forum/service/UserDetail.kt
@@ -4,7 +4,7 @@ import br.com.alura.forum.model.Usuario
 import org.springframework.security.core.userdetails.UserDetails
 
 class UserDetail(private val usuario: Usuario) : UserDetails {
-    override fun getAuthorities() = null
+    override fun getAuthorities() = usuario.role
 
     override fun getPassword() = usuario.password
 
